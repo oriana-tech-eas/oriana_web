2650591a4cd2a034513eb407d7b8461ae3f9829f
Anderson Fariña <inumansoul@gmail.com>
2025-04-03 22:21:05 -0300

    logo update for login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   return (
     <main className="h-dvh w-full flex items-center justify-center bg-gradient-to-tl from-neutral-100 dark:from-neutral-800 to-neutral-50 dark:to-neutral-950">
       <a href="/">
-        <Image src="/brand/oriana-market.svg" alt="Printit" className="absolute top-5 left-5 object-contain" width={120} height={36} />
+        <Image src="/brand/oriana-tech.svg" alt="Printit" className="absolute top-5 left-5 object-contain" width={120} height={36} />
       </a>
       {children}
     </main>
